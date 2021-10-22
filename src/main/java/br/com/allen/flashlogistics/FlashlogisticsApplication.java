614eb1de6cd20839bfeed59e97894a651170af33
AllenLVieira <allenvieira96@gmail.com>
2021-10-22 00:03:19 -0300

    chore: initial commit of project

---
+++ src/main/java/br/com/allen/flashlogistics/FlashlogisticsApplication.java
@@ -0,0 +1,13 @@
+package br.com.allen.flashlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashlogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashlogisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/allen/flashlogistics/FlashlogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allen.flashlogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashlogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
